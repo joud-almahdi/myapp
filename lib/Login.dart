32dd2e5c5c9f5a9045298d62433f0c216e70d936
joud-almahdi <joudalmahdi7@gmail.com>
2022-10-05 10:14:53 +0300

    changes for Firebase auth

---
+++ lib/Login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+ß
